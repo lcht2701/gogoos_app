2e4b12c5d4ee407292a06dbfdca2f4c85b2954a7
lcht2701 <lecanhhoangthanh@gmail.com>
2023-07-14 10:00:52 +0700

    revert code

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:gogoos_app/views/utils/app_color.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
-import '../../api/purchase_api.dart';
 import '../../controllers/auth_controller.dart';
 import '../../controllers/user_controller.dart';
 import '../utils/my_recipes_tab.dart';
@@ -21,19 +20,6 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   //current user
   final currentUser = FirebaseAuth.instance.currentUser!;
-
-  Future fetchOffer() async {
-    final offerings = await PurchaseApi.fetchOffers();
-
-    if (offerings.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('No Plans Found'),
-      ));
-    } else {
-      final offer = offerings.first;
-      debugPrint('Offer: $offer');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
